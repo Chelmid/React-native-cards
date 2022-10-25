46776848e62baccccf94b173330fb075f44056a0
Michel <lo.chelmi@gmail.com>
2022-10-25 10:35:43 +0200

    Revert "test expo apk"
    This reverts commit 3fc7e7c725bde3a15ef602016b56c641448b222a.

--- App.js
+++ App.js
@@ -1,34 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import * as Google from 'expo-auth-session/providers/google';
-import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
-    iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
-    androidClientId: '781347252583-erv92apa8966angup055nic1gar37i7n.apps.googleusercontent.com',
-    webClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
-  });
-
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { authentication } = response;
-    }
-  }, [response]);
-
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Button
-      disabled={!request}
-      title="Login"
-      onPress={() => {
-        promptAsync();
-      }}
-    />
     </View>
   );
 }
